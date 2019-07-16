f6ca059b2214b9fa95151f25f6cd8a3f009421ef
transoman <vasiag10@gmail.com>
2019-07-16 12:55:11 +0300

    Bugs fix

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -30,7 +30,7 @@ module.exports = function () {
           ]
         }),
         gp.imagemin.optipng({optimizationLevel: 3}),
-        pngquant({quality: '65-70', speed: 5})
+        pngquant({quality: [0.65, 0.7], speed: 5})
       ], {
         verbose: true
       })))
